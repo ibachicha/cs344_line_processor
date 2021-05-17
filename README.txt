d37fda95c282c38ff49bf3bbeb5c5253d597ab16
isaac bachicha <isaacbachicha1@gmail.com>
2021-05-16 19:26:00 -0700

    Final 5/16
    Updated and finalized w/comments

---
+++ README.txt
@@ -0,0 +1,19 @@
+TO COMPLILE PROGRAM:
+
+gcc --std=gnu99 -o line_processor -g -Wall line_processor.c -pthread
+
+TO RUN PROGRAM:
+
+./line_processor
+
+TO RUN WITH INPUT REDIRECTION:
+
+./line_processor < INPUTFILE_NAME
+
+TO RUN WITH OUTPUT REDIRECTION:
+
+./line_processor > OUTPUTFILE_NAME
+
+TO RUN WITH INPUT AND OUTPUT REDIRECTION:
+
+./line_processor < INPUTFILE_NAME > OUTPUTFILE_NAME
--- line_processor.c
+++ line_processor.c
@@ -13,6 +13,9 @@ char buffer2[NUM_LINES][LINE_LENGTH] = {{0}};
 char buffer3[NUM_LINES][LINE_LENGTH] = {{0}};
 
 
+// PLEASE NOTE: GLOBALS ARE LARGELY TAKEN FROM THE 
+// EXPLORATIONS WITH A FEW MODIFICATIONS.
+
 // BUFFER 1
 int prod_idx_1 = 0;
 int cond_idx_1 = 0;
@@ -42,28 +45,25 @@ pthread_mutex_t mutex_3 = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t full_3 = PTHREAD_COND_INITIALIZER;
 
 
-// Thread 4
-//char print_buffer[50*1024];
-//char print_buffer[NUM_LINES][LINE_LENGTH] = {{0}};
-//char print_buffer[5000] = {{0}};
-int print_idx = 0;
-int print_count = 0;
+// THREAD 4 GLOBAL
 int printBuffer_size = 0;
-int string_count = 0;
 
 
+// PLEASE NOTE: GET AND PUT BUFFERS ARE LARGELY TAKEN FROM THE 
+// EXPLORATIONS WITH A FEW MODIFICATIONS. 
 
-
+// Consumer Side
 void get_buff_1(char *item){
     // Lock the mutex before putting the item in the buffer
     pthread_mutex_lock(&mutex_1); //shared resource
     while(count_1 == 0)
     {
-        pthread_cond_wait(&full_1, &mutex_1); //if nothing in buffer, wait until producer tells us something there
+        //if nothing in buffer, wait until producer tells us something there
+        pthread_cond_wait(&full_1, &mutex_1);
     }
     // Put the item in the buffer
     strcpy(item, buffer1[cond_idx_1]); 
-    // Increment the index where the next item will be put.
+    // Decrement the index where we are getting.
     cond_idx_1 = cond_idx_1 + 1;
     count_1--;
     // Unlock the mutex
@@ -71,10 +71,7 @@ void get_buff_1(char *item){
 
 }
 
-/*
- Put an item in buff_1
-*/
-//Production side of buffer 1 -- still needs consumer side of buff 1
+// Production side of buffer 1
 void put_buff_1(char *item){
     // Lock the mutex before putting the item in the buffer
     pthread_mutex_lock(&mutex_1);
@@ -91,14 +88,15 @@ void put_buff_1(char *item){
 
 void get_buff_2(char *item){
     // Lock the mutex before putting the item in the buffer
-    pthread_mutex_lock(&mutex_2); //shared resource
+    pthread_mutex_lock(&mutex_2); 
     while(count_2 == 0)
     {
-        pthread_cond_wait(&full_2, &mutex_2); //if nothing in buffer, wait until producer tells us something there
+        //if nothing in buffer, wait until producer tells us something there
+        pthread_cond_wait(&full_2, &mutex_2);
     }
     // Put the item in the buffer
     strcpy(item, buffer2[cond_idx_2]); 
-    // Increment the index where the next item will be put.
+    // Decrement the index where we are getting.
     cond_idx_2 = cond_idx_2 + 1;
     count_2--;
     // Unlock the mutex
@@ -125,11 +123,12 @@ void get_buff_3(char *item){
     pthread_mutex_lock(&mutex_3); //shared resource
     while(count_3 == 0)
     {
-        pthread_cond_wait(&full_3, &mutex_3); //if nothing in buffer, wait until producer tells us something there
+        //if nothing in buffer, wait until producer tells us something there
+        pthread_cond_wait(&full_3, &mutex_3); 
     }
     // Put the item in the buffer
     strcpy(item, buffer3[cond_idx_3]); 
-    // Increment the index where the next item will be put.
+    // Decrement the index where we are getting.
     cond_idx_3 = cond_idx_3 + 1;
     count_3--;
     // Unlock the mutex
@@ -151,76 +150,42 @@ void put_buff_3(char *item){
     pthread_mutex_unlock(&mutex_3);
 }
 
-char* expandToken(char* token) {
+// Expand the token from ++ for ^
+char* expandToken(char* token) 
+{
     char* term = "++";
 	char *replace = "^";
 	
-    if (!strstr(token, term)) {
+    if (!strstr(token, term)) 
+    {
         return token;
     }
+    // else ++ is found
     char* buf = malloc(strlen(token) + 1);
     char* p = NULL;
-    char* rest = token;    
-    while ((p = strstr(rest, term))) {
-        //printf("This is rest: %s  This is term: %s \n", rest, term);
+    char* rest = token; 
+        
+    // while a "++" is present in the string
+    while ((p = strstr(rest, term)))
+    {
+        // Move everything before the ++ to the buffer
         strncat(buf, rest, (size_t)(p - rest));
+        // Append the caret onto the buffer in the place of ++
         strcat(buf, replace);
-        rest = p + (strlen(replace) + 1); //maybe the plus 1 helps
-        //printf("This is rest2: %s  This is P: %s This is replace: %s\n", rest, p, replace);
-    }    
+        // rest is now everything after the first ++ is found. Loop back to check for more
+        rest = p + (strlen(replace) + 1);
+    }
+    // If no more "++" found, append what's leftover in rest to the buffer  
     strcat(buf, rest);
     return buf;
 }
 
-// Print Buffer
-void pb(char b[NUM_LINES][LINE_LENGTH]) 
+// producer thread -  get the input
+void *t1(void *args)
 {
-    for(int i = 0; i < NUM_LINES; i++)
-    {
-        if(b[i][0] != '\0')
-        {
-            printf("%d | %s\n", i, b[i]);
-        }
-        else
-        {
-            break;
-        }
-    }
-}
-
-void print_t4(char p4[NUM_LINES][LINE_LENGTH])
-{
-int cache_i = 0;
-int cache_j = 0;
-
-int count = 0;
-for(int i = cache_i; i < NUM_LINES ; i++)
-{
-    for ( int j = cache_j; j < LINE_LENGTH ; j++)
-    {
-        printf("THREAD 4: %c\n", p4[i][j]);
-        count++;
-
-        if(count >= 80)
-        {
-            count = 0;
-            printf("\n");
-            cache_i = i;
-            cache_j = j;
-        }
-        else
-        {
-            break;
-        }
-    }
-}
-}
-
-
-// producer thread
-void *t1(void *args){
     int stop = 0;
     char *input = malloc(1024);
+
     while(!stop){
         fgets(input, 1024, stdin);
         if(strcmp(input, "STOP\n") == 0){
@@ -231,10 +196,12 @@ void *t1(void *args){
     return NULL;
 }
 
-
-void *t2(void *args){
+// remove the new line
+void *t2(void *args)
+{
     int stop = 0;
     char *input = malloc(1024);
+
     while(!stop)
     {
         get_buff_1(input); // get stuff out of buff 1
@@ -244,29 +211,31 @@ void *t2(void *args){
         }
         else
         {
-            input[strcspn(input, "\n")] = 0; //remove /n // need to add a space I believe
-            strcat(input, " ");
+            input[strcspn(input, "\n")] = 0; // remove new line
+            strcat(input, " ");  // add a space
         }
         put_buff_2(input); // put output into buffer 2
     }
     return NULL;
 }
 
-void *t3(void *args){
+// check for "++" in input
+void *t3(void *args)
+{
     int stop = 0;
     char *input = malloc(1024);
+
     while(!stop)
     {
-        get_buff_2(input); // get stuff out of buff 1
+        get_buff_2(input); // get stuff out of buff 2
         if(strcmp(input, "STOP\n") == 0)
         {
             stop = 1;
         }
+        // Send to check for ++ to possible expand into ^
         char* expanded = expandToken(input);
         strcpy(input, expanded);
-        // strcat(input, "*");
-        // convert ++ to caret
-        put_buff_3(input); // put output into buffer 2
+        put_buff_3(input); // put output into buffer 3
     }
     return NULL;
 }
@@ -275,64 +244,52 @@ void *t4(void *args){
     int stop = 0;
     char *input = malloc(1024);
     char print_buffer[50*1024];
-    int arr_count = 0;
+    int line_char_count = 0;
     int cache_j;
-    //int string_count = 0;
-    
     int last_line_printed = -1;
-    //int printBuffer_size; // probably set to global
 
     while(!stop)
     {
         get_buff_3(input); // get stuff out of buff 3
-        strcat(print_buffer, input);
+        strcat(print_buffer, input); // cat input to print buffer
         if(strcmp(input, "STOP\n") == 0)
         {
             stop = 1;    
         }
-        // strcpy(print_buffer[print_idx], input);
-        // print_idx = print_idx + 1;
+        // get the length of the buffer
         printBuffer_size += strlen(input);
-        
-        // printf("\n");
-        // printf("Buffer is of %d size.\n", printBuffer_size);
-        // printf("\n");
      
+        // Loop though the entire buffer
         for(int j = cache_j; j < printBuffer_size; j++)
         {
-            // printf("\n");
-            // printf("Starting J is: %d, starting Arr_count is: %d\n", j, arr_count);
-            // printf("Starting Character is: %c\n", print_buffer[j]);
-            // printf("\n");
-            arr_count += 1;
+            // increment the character count for the line
+            line_char_count += 1;
             
-            if(arr_count == 80)
+            if(line_char_count == 80)
             {
-                int end_of_line = arr_count + last_line_printed;
+                int end_of_line = line_char_count + last_line_printed;
                 last_line_printed = last_line_printed + 1;
-                // printf("we are in here\n");
-                //printf("end of file is: %d Last Line Printed is: %d\n", end_of_line, last_line_printed);
+
+                // print each character starting at the last line printed
+                // and increment until end of line is hit
                 while(last_line_printed <= end_of_line)
                 {
-                    // printf("\n");
-                    // printf("This is the character string:\n");
                     printf("%c", print_buffer[last_line_printed]);
-                    // printf("\n");
                     last_line_printed += 1;
                 }
-                printf("\n");   
+                // 80 characters have been reached so
+                // ...set the last line
+                // ...reset the character count for the line
+                printf("\n");  
                 last_line_printed = j;
-                arr_count = 0;
+                line_char_count = 0;
 
             }
-            //last_line_printed = j + 1;
-            cache_j = j + 1;
-            // printf("Ending J is: %d, (Cache J = %d) Ending Arr_count is: %d\n", j, cache_j, arr_count);
-            
-        }
-        
+        // next
+        cache_j = j + 1;    
+        }   
     }
-    return NULL;
+return NULL;
 }
 
   
@@ -348,14 +305,11 @@ int main()
     pthread_create(&linesep_t, NULL, t2, NULL);
     pthread_create(&replacePlus_t, NULL, t3, NULL);
     pthread_create(&output_t, NULL, t4, NULL);
+    
     pthread_join(input_t, NULL);
     pthread_join(linesep_t, NULL);
     pthread_join(replacePlus_t, NULL);
     pthread_join(output_t, NULL);
-    // pb(buffer1);
-    // pb(buffer2);
-    // pb(buffer3);
-    //pb(print_buffer);
 
     return 0;
 }
